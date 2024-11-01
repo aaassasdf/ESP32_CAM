8a8d1384bbb61188e161c65e947848ad5bb8c01b
Michael Lam <michaellam1996@gmail.com>
2024-11-01 20:30:59 +0800

    Update connect_wifi.c

--- main/connect_wifi.c
+++ main/connect_wifi.c
@@ -5,8 +5,8 @@ static const char *TAG = "Connect_WiFi";
 int s_retry_num = 0;
 
 
-#define WIFI_SSID "JC"
-#define WIFI_PASSWORD "XMG3-Rel.1"
+#define WIFI_SSID "xxxxx"
+#define WIFI_PASSWORD "xxxxx"
 #define MAXIMUM_RETRY 5
 /* FreeRTOS event group to signal when we are connected*/
 EventGroupHandle_t s_wifi_event_group;
